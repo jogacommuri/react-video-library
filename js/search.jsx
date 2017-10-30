65b4be66913e1ba24bc935652b618358224e0ee0
Joga Commuri <jogacommuri@yahoo.com>
2017-10-30 01:21:24 -0700

    showcard component added

--- js/search.jsx
+++ js/search.jsx
@@ -1,5 +1,11 @@
 import React from 'react';
+import preload from '../data.json';
+import ShowCard from './showCard';
 
-const Search = () => <h1>hi lol </h1>;
+const Search = () => (
+	<div className="search">
+		<div>{preload.shows.map(show => <ShowCard key={show.imdbID} {...show} />)}</div>
+	</div>
+);
 
 export default Search;
